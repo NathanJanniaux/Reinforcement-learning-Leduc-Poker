46d470bf27894947aa37de7b484205e23debb574
Nathan janniaux <nathan.barranco-janniaux@telecom-st-etienne.fr>
2020-05-20 12:44:12 +0200

    test not implemented

--- utils.py
+++ utils.py
@@ -15,14 +15,26 @@ def choose_and_remove(items):
     # nothing left!
     return None
 
-def get_max_list(l):
+def get_max_list(l, allowed_actions):
     index=None
-    if(l[0]==l[1]):
-        index=random.randrange(0,2)
-    elif(l[0]>l[1]):
-        index=0
-    else:
-        index=1
+    if (len(allowed_actions)==2):
+        l.remove('1')
+        if (l[0]>=l[1]):
+           index=0
+        else :
+           index=2
+    elif (len(allowed_actions)==3):
+        if (l[0]>=l[1]):
+            if (l[0]>= l[2]):
+                index=0
+            elif:
+                index=2
+        elif(l[0]<=l[1]):
+            if (l[1]>=l[2]):
+                index=1
+            else:
+                index=2
+        
     return(index)
 
 
